853d25fcbe2cfb4ab2f78d33155e8763d592542f
Anshit-Sony <156159118+Anshit-Sony@users.noreply.github.com>
2024-04-20 11:15:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slider=document.querySelector(".sliderBall")
+let currmode="dark"
+let root=document.querySelector(':root');
+let mode=document.querySelector("#slideball")
+let contact=document.querySelector("#contact")
+slider.addEventListener("click",()=>{
+    if(currmode==="dark"){
+        slider.classList.add("slide")
+        root.style.setProperty("--bg-color","#f5e9e9");
+        root.style.setProperty("--nav-bg-color","rgb(159, 170, 150)");
+        root.style.setProperty("--txt-col","rgb(21, 20, 20)");
+        mode.innerHTML="<i class=\"fa-solid fa-sun\"></i>";
+        contact.style.backgroundColor="rgb(225 213 213)"
+        currmode="light"
+    }else{
+        slider.classList.remove("slide")
+        root.style.setProperty("--bg-color","#232323");
+        root.style.setProperty("--nav-bg-color","rgb(88, 98, 80)");
+        root.style.setProperty("--txt-col","white");
+        mode.innerHTML="<i class=\"fa-solid fa-moon\"></i>";
+        contact.style.backgroundColor="rgb(46, 45, 45)"
+        currmode="dark"
+    }
+})
